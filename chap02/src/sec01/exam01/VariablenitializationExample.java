01730462ef9f22e726748ef4792136057b6f6df5
rmflsms <rmflsms>
2021-11-03 22:43:48 +0900

    변수 공부

---
+++ chap02/src/sec01/exam01/VariablenitializationExample.java
@@ -0,0 +1,13 @@
+package sec01.exam01;
+
+public class VariablenitializationExample {
+
+	public static void main(String[] args) {
+		int value = 30;
+		
+		int result = value + 10;
+
+		System.out.println(result);
+	}
+
+}
