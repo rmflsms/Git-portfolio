0e7d3955781ddeda7679abe6a43b03da22725c60
rmflsms <rmflsms>
2021-11-11 23:16:21 +0900

    기본타입 예제 공부

---
+++ chap02/src/sec02/exam02/ByteExample.java
@@ -0,0 +1,19 @@
+package sec02.exam02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		
+		byte var1 = -128;
+		byte var2 = 0;
+		byte var3 = 127;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	
+		
+
+	}
+
+}
