7fc8cbd74ae56126d1171644e65dea0f37235719
rmflsms <rmflsms>
2021-12-28 00:05:25 +0900

    변수 타입 변환 (2)

---
+++ chap02/src/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,24 @@
+package sec03.exam03;
+
+public class ByteOperationExample {
+
+	public static void main(String[] args) {
+		
+		
+		byte result1 = 10 + 20 ;
+		
+	
+		byte x = 10;
+		byte y = 20; 
+		int result = x + y;
+		
+		byte x2 = 30;
+		long y2 = 40;
+		long result2 = x2 + y2 ;
+		
+		
+
+		
+	}
+
+}
