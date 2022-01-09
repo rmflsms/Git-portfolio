582d2d1b34fed16a508ebbd301257c54dbf00334
rmflsms <rmflsms>
2022-01-10 01:00:55 +0900

    연산자의 종류

---
+++ chap03/src/sec02/exam03/DenyLogicOperatorExakple.java
@@ -0,0 +1,17 @@
+package sec02.exam03;
+
+public class DenyLogicOperatorExakple {
+
+	public static void main(String[] args) {
+		
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+	}
+
+}
